aaa9fccebd88cbf656b1f78f18ee9a6dbf6317d2
Tanishq <tanishqmlzs786@gmail.com>
2024-06-06 15:11:38 +0530

    new upadates

--- trialCode.java
+++ trialCode.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class trialCode {
-
     public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            System.out.println("enter string name");
+            System.out.println("enter name of string");
             String name = sc.nextLine();
             System.out.println(name);
             System.out.print("no. of letter in string is" + name.length());
