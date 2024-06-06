3e01a8282f32dfe029019864338bbe88e9e473ff
Tanishq <tanishqmlzs786@gmail.com>
2024-06-06 12:46:05 +0530

    diplay length of string

--- trialGit.java
+++ trialGit.java
@@ -3,8 +3,10 @@ public class trialGit {
 
     public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
+            System.out.println("enter string name");
             String name = sc.nextLine();
-            System.out.print(name);
+            System.out.println(name);
+            System.out.print("no. of letter in string is" + name.length());
     }
 
 }
